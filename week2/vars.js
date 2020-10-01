95c43c937b666a7da09265eba1af1a989ab40c2e
trossossp3 <21trossos@gmail.com>
2020-10-01 17:34:50 -0400

    lesson plans

---
+++ week2/vars.js
@@ -0,0 +1,16 @@
+var a = 'Hi there!';  
+var b = "Hi there!";  
+
+
+var n1 = 25;         // integer
+var n2 = 80.5;       // floating-point number
+
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping
+
+var colors = ["Red", "Yellow", "Green", "Orange"];
+var cities = ["London", "Paris", "New York"];
+
+
+alert(colors[0]);   // Output: Red
+alert(cities[2]);   // Output: New York
